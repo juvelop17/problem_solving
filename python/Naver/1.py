330a49a96829cdcb2b9b0460fa180dc0128ccb2e
juvelop <juvelop17@gmail.com>
2019-09-26 00:41:22 +0900

    python commit

---
+++ python/Naver/1.py
@@ -0,0 +1,10 @@
+li = [1,2,3]
+
+print(li)
+li.pop(-1)
+print(li)
+
+
+li = ['a']
+
+print(li=='a')
---
+++ python/input_output_test.py
@@ -0,0 +1,10 @@
+import sys
+
+sys.stdin = open('input.txt', 'r')  # console input
+# sys.stdout = open('output.txt', 'w')  # console output
+
+t = sys.stdin.readline() # input 보다 빠름
+print(t)
+
+# print(input())
+# print(input())
