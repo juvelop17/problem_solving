ca9bf03131e1da75d578622c54045be61131a8dd
juvelop <juvelop17@gmail.com>
2019-10-05 16:14:56 +0900

    191005

---
+++ C/BOJ/!_9465.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+    int N;
+
+    freopen("input.txt", "r", stdin);  
+
+    scanf("%d",&N);
+    printf("%d",N);
+
+    return 0;
+}
+
+
+
---
+++ C/input_output.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+    int N;
+
+    freopen("input.txt", "r", stdin);  
+    freopen("output.txt", "w", stdout);  
+
+    scanf("%d",&N);
+    printf("%d",N);
+
+    return 0;
+}
+
+
+
