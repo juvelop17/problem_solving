1ec4934491fac9919c2a6fa3f5ceebca50f905df
juvelop17 <juvelop17@gmail.com>
2019-10-30 22:49:18 +0900

    swea

--- C/SWEA/!8659.cpp
+++ C/SWEA/!8659.cpp
@@ -1,21 +1,29 @@
-#include <stdio.h>
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
 
 
 int T,K;
 int MAX_VAL = 100000;
 int min_a = MAX_VAL;
 int min_b = MAX_VAL;
-int prime_num[10000] = {0};
 
+int map_arr[100000][100000] = {0};
+// int prime_num[10000] = {0};
 
-void getPrimeNum(){
-    
-}
 
 
 int GCD(int n, int a,int b){
-    if (b == 0){
+    int res;
+    if (map_arr[a][b] == -1) {
         return -1;
+    } else if (map_arr[a][b] != 0) {
+        return map_arr[a][b];
+    }
+
+    if (b == 0){
+        return 0;
     }
 
     if (n == K){
@@ -26,7 +34,12 @@ int GCD(int n, int a,int b){
         }
     }
 
-    return GCD(n+1,b,a%b);
+    res = GCD(n+1,b,a%b);
+    if (res == -1){
+        return -1;
+    }
+
+    return res + 1;
 }
 
 
@@ -35,15 +48,11 @@ int solution(){
     min_a = MAX_VAL;
     min_b = MAX_VAL;
 
-    for (int i=2;i<=MAX_VAL;i++){
-        // printf("%d",i);
-        if (i > min_a){
-            break;
-        }
-
+    for (int i=2;i<=min_a;i++){
+        cout << i;
         for (int j=1;j<i;j++){
             res = GCD(1,i,j);
-            // printf("%d",res);
+            cout << res;
             if (res != -1){
                 min_a = i;
                 min_b = j;
@@ -56,13 +65,17 @@ int solution(){
 
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     freopen("input.txt","r",stdin);
 
-    scanf("%d",&T);
+    cin >> T;
     for (int i=1;i<=T;i++){
-        scanf("%d",&K);
+        cin >> K;
+        cout << K;
         solution();
-        printf("#%d %d %d\n",i,min_a,min_b);
+        cout << i << min_a << min_b;
     }
 
     return 0;
