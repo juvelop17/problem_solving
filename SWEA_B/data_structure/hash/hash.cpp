f1999f0a9257bfeb843ee8860e5e7fee0cdd1e05
juvelop17 <juvelop17@gmail.com>
2019-11-22 15:03:19 +0900

    swea stl

--- SWEA_B/data_structure/hash/hash.cpp
+++ SWEA_B/data_structure/hash/hash.cpp
@@ -3,58 +3,20 @@
 using namespace std;
 
 
-
-template <class K, class V>
-class map{
-public:
-    struct Node{
-        Node *l, *r, *p;
-        K key;
-        T val;
-    }
-    
-    Node *root;
-    map():root(0){}
-    ~map(){
-        while (root) erase(root->key);
-    }
-    void rotate(Node *x){
-        
-    }
-    
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-#define MAX_TABLE 5
-#define MAX_KEY 8
-#define MAX_DATA 12
+#define MAX_KEY 64
+#define MAX_DATA 128
+#define MAX_TABLE 4096
 
 #define DELETE_COUNT 6
 #define FIND_COUNT 8
 
 
 struct Node {
-    char key[MAX_KEY];
+    char key[MAX_KEY + 1];
     int value;
     Node *next;
 };
-
 Node *tb[MAX_TABLE];
-char keys[MAX_DATA][MAX_KEY];
-int values[MAX_DATA];
-
 
 void init(){
     for(int i=0;i<MAX_TABLE;++i){
@@ -71,7 +33,7 @@ void init(){
     // 미완성
 }
 
-void my_str_cpy(char *dest, const char *src){
+void my_strcpy(char *dest, const char *src){
     while(*src != '\0') {
         *dest = *src;
         dest++; src++;
@@ -79,7 +41,7 @@ void my_str_cpy(char *dest, const char *src){
     *dest = '\0';
 }
 
-int my_str_cmp(const char *str1, const char *str2){
+int my_strcmp(const char *str1, const char *str2){
     while (*str1 != '\0' && (*str1 == *str2)) {
         str1++;
         str2++;
@@ -87,47 +49,25 @@ int my_str_cmp(const char *str1, const char *str2){
     return *str1 - *str2;
 }
 
-int hash(const char *str) {
-    int hash = 401;
+
+int getHash(const char *str) {
+    int hash = 5381;
     int c;
 
-    while (*str != '\0') {
-        hash = ((hash << 4) + (int)(*str)) % MAX_TABLE;
+    while (c = *str++) {
+        hash = (((hash << 5) + hash) + c) % MAX_TABLE;
         str++;
     }
 
     return hash % MAX_TABLE;
 }
 
-void add(const char *key, int value){
-    Node *new_node = new Node;
-    my_str_cpy(new_node->key,key);
-    new_node->value = value;
-    new_node->next = NULL;
-
-    int index = hash(key);
-
-    if(tb[index] == NULL){
-        tb[index] = new_node;
-    } else {
-        Node *cur = tb[index];
-        while (cur != NULL) {
-            if(my_str_cmp(cur->key,key) == 0) {
-                cur->value = value;
-                return;
-            }
-            cur = cur->next;
-        }
-        new_node->next = tb[index];
-        tb[index] = new_node;
-    }
-}
 
 bool find(const char *key, int *val){
-    int index = hash(key);
-    Node *cur = tb[index];
+    int h = getHash(key);
+    Node *cur = tb[h];
     while (cur != NULL) {
-        if (my_str_cmp(cur->key,key) == 0){
+        if (my_strcmp(cur->key,key) == 0){
             *val = cur->value;
             return true;
         }
@@ -137,10 +77,107 @@ bool find(const char *key, int *val){
     return false;
 }
 
-bool destory(const char *key) {
-    int index = hash(key);
 
-    if (tb[index] == )
+void add(const char *key, int value){
+    Node *new_node = new Node;
+    my_strcpy(new_node->key,key);
+    new_node->value = value;
+    new_node->next = NULL;
+
+    int h = getHash(key);
+
+    if(tb[h] == NULL){
+        tb[h] = new_node;
+    } else {
+        Node *cur = tb[h];
+        while (cur != NULL) {
+            if(my_strcmp(cur->key,key) == 0) {
+                cur->value = value;
+                return;
+            }
+            cur = cur->next;
+        }
+        new_node->next = tb[h];
+        tb[h] = new_node;
+    }
+}
+
+
+bool destroy(const char *key){
+    int h = getHash(key);
+
+    if(tb[h] == NULL) {
+        return false;
+    }
+
+    if (my_strcmp(tb[h]->key,key) == 0){
+        Node *first = tb[h];
+        tb[h] = tb[h]->next;
+        free(first);
+        return true;
+    } else {
+        Node *cur = tb[h]->next;
+        Node *prev = tb[h];
+
+        while (cur != NULL && my_strcmp(cur->key,key) != 0){
+            prev = cur;
+            cur = cur -> next;
+        }
+
+        if (cur == NULL) return false;
+
+        prev->next = cur->next;
+        free(cur);
+        return true;
+    }
+}
+
+
+
+
+int main(int argc, char* argv[])
+{
+    int T, N, Q;
+    freopen("input.txt","r",stdin);
+
+    cin >> T;
+ 
+    for (int test_case = 1; test_case <= T; test_case++)
+    {
+        memset(tb, 0, sizeof(tb));
+        cin >> N;
+        char k[MAX_KEY + 1];
+        // char d[MAX_DATA + 1];
+        int val;
+ 
+        for (int i = 0; i < N; i++)
+        {
+            cin >> k >> val;
+            add(k, val);
+        }
+ 
+        cout << "#" << test_case << endl;
+ 
+        cin >> Q;
+        for (int i = 0; i < Q; i++)
+        {
+            char k[MAX_KEY + 1];
+            // char d[MAX_DATA + 1];
+            int val;
+ 
+            cin >> k;
+ 
+            if (find(k, &val))
+            {
+                cout << val << endl;;
+            }
+            else
+            {
+                cout << "not find\n";
+            }
+        }
+    }
+    return 0;
 }
 
 
