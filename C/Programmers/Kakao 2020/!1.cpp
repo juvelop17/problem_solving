40ec5899d2762c51ac0a202434fafc7c82655f92
juvelop17 <juvelop17@gmail.com>
2020-09-07 10:52:58 +0900

    200907

---
+++ C/Programmers/Kakao 2020/!1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    string comp;
+    string tmp;
+
+    for(int i=1;i<=s.length()/2;i++){
+        strcpy(tmp,s.subs)
+    }
+
+
+    return answer;
+}
+
+int T;
+string S;
+
+int main() {
+    freopen("input.txt","r",stdin);
+
+    cin >> T;
+    
+    for(int i=0;i<T;i++){
+        cin >> S;
+        cout << S << endl;
+        cout << solution(S) << endl;
+    }
+
+    return 0;
+}
+
